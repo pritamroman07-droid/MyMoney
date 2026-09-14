6d376a0e6aba5c1739eb24507504df5013a64f25
pritamroman07-droid <pritamroman07@gmail.com>
2026-09-14 12:26:37 +0530

    register api done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import authRoutes from './routes/authRoutes.js'
 import transactionRoutes from './routes/transactionRoutes.js'
+import incomeRoutes from './routes/incomeRoutes.js'
+import budgetRoutes from './routes/budgetRoutes.js'
 
 dotenv.config()
 
@@ -16,7 +19,10 @@ app.get('/', (req, res) => {
   res.json({ message: 'MyMoney API is running' })
 })
 
+app.use('/api/auth', authRoutes)
 app.use('/api/transactions', transactionRoutes)
+app.use('/api/income', incomeRoutes)
+app.use('/api/budget', budgetRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
